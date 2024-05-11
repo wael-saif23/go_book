2f4676d5e0c72272c94b6d9e44a0006e00c8747f
wael-saif23 <ws.mywork@gmail.com>
2024-05-12 01:04:20 +0300

    Refactor SplashViewBody widget and add AnimatedSlideText widget
    Refactored SplashViewBody widget to be a StatefulWidget and added AnimatedSlideText
    widget for sliding text animation during splash screen loading.

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,30 +1,70 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:go_book/core/utils/assets.dart';
-class SplashViewBody extends StatelessWidget {
+
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
-                                          
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slideAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    slideAnimation =
+        Tween<Offset>(begin: const Offset(0, 15), end: const Offset(0, 0))
+            .animate(animationController);
+            animationController.forward();
+  
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        const Spacer(
-          flex: 1,
-        ),
         Center(
           child: Image.asset(AssetsData.logo),
         ),
-        const Spacer(
-          flex: 1,
-        ),
         const Text('Read and buy books', style: TextStyle(fontSize: 15)),
-        const Text('Loading...', style: TextStyle(fontSize: 10)),
+        AnimatedSlideText(slideAnimation: slideAnimation),
         const SizedBox(
           height: 20,
         )
       ],
     ));
+  }
+}
+
+class AnimatedSlideText extends StatelessWidget {
+  const AnimatedSlideText({
+    super.key,
+    required this.slideAnimation,
+  });
+
+  final Animation<Offset> slideAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      builder: (context, child) {
+        return SlideTransition(
+            position: slideAnimation,
+            child:
+                const Text('Loading...', style: TextStyle(fontSize: 10)));
+      },
+      animation: slideAnimation,
+    );
   }
 }
