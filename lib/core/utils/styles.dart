3919f00ca766a6333a9424cc01d5908fa0d02616
wael-saif23 <ws.mywork@gmail.com>
2024-05-12 11:06:08 +0300

    Add text style for best seller list in home view body
    - Define a text style for the "Best Seller List" text in the home view body
      using the `Styles` class from `styles.dart`.
    - Update the `HomeViewBody` widget to use the defined text style for the
      "Best Seller List" text.

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const textMediam = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:go_book/core/utils/styles.dart';
 import 'package:go_book/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:go_book/features/home/presentation/views/widgets/featured_list_view.dart';
-
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(children: [
-      CustomAppBar(),
-      FreaturedListView(),
-    ]);
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(children: [
+        CustomAppBar(),
+        FreaturedListView(),
+        SizedBox(
+          height: 8,
+        ),
+        Row(
+          children: [
+            Text("Best Seller List", style: Styles.textMediam),
+          ],
+        ),
+      ]),
+    );
   }
 }
