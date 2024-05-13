c610b96034b7619af97ca0f607d1fd63bebf21f0
wael-saif23 <ws.mywork@gmail.com>
2024-05-14 00:20:28 +0300

    Add Failure class and its subclasses, create HomeRepo interface
    - Add Failure class and its subclasses to handle different types of failures.
    - Create HomeRepo interface with methods to fetch best sellers and featured books.

---
+++ lib/core/errors/failures.dart
@@ -0,0 +1,43 @@
+abstract class Failure {}
+
+class ServerFailure extends Failure {}
+
+// class CacheFailure extends Failure {}
+
+// class OfflineFailure extends Failure {}
+
+// class UnknownFailure extends Failure {}
+
+// class InvalidInputFailure extends Failure {}
+
+// class NetworkFailure extends Failure {}
+
+// class NoInternetFailure extends Failure {}
+
+// class UnAuthorizedFailure extends Failure {}
+
+// class ForbiddenFailure extends Failure {}
+
+// class BadRequestFailure extends Failure {}
+
+// class NotFoundFailure extends Failure {}
+
+// class ConflictFailure extends Failure {}
+
+// class InternalServerErrorFailure extends Failure {}
+
+// class ServiceUnavailableFailure extends Failure {}
+
+// class BadGatewayFailure extends Failure {}
+
+// class GatewayTimeoutFailure extends Failure {}
+
+// class TooManyRequestsFailure extends Failure {}
+
+// class UnprocessableEntityFailure extends Failure {}
+
+// class UnknownErrorFailure extends Failure {}
+
+// class UnKnownFailure extends Failure {}
+
+// class NoDataFailure extends Failure {}
---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:go_book/core/errors/failures.dart';
+import 'package:go_book/features/home/data/models/book_model/book_model/book_model.dart';
+
+abstract class HomeRepo {
+ Future<Either<Failure, List<BookModel>>> fetchBestSellersBooks();
+ Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+}
