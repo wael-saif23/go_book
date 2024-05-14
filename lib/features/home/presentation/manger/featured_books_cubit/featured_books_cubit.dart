a44c93fb60db14ac502cfae2987a3518bba0179d
wael-saif23 <ws.mywork@gmail.com>
2024-05-14 23:35:44 +0300

    Add fetchFeaturedBooks method to FeaturedBooksCubit
    Added fetchFeaturedBooks method to FeaturedBooksCubit to fetch featured books
    from HomeRepo and update state accordingly.

--- lib/features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:go_book/features/home/data/models/book_model/book_model/book_model.dart';
+import 'package:go_book/features/home/data/repos/home_repo.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit({required this.homeRepo}) : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(FeaturedBooksfailure(error: failure.errorMessage));
+    }, (books) {
+      emit(FeaturedBooksSuccess(books: books));
+    });
+  }
 }
