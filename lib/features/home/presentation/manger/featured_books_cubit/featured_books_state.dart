50cfc54b8a6fa80ccae0ead0396c6013f2129429
wael-saif23 <ws.mywork@gmail.com>
2024-05-14 22:33:53 +0300

    Add FeaturedBooksCubit and FeaturedBooksState classes
    Added FeaturedBooksCubit and FeaturedBooksState classes with initial, loading,
    success, and failure states for managing featured books in the home screen.

---
+++ lib/features/home/presentation/manger/featured_books_cubit/featured_books_state.dart
@@ -0,0 +1,23 @@
+part of 'featured_books_cubit.dart';
+
+sealed class FeaturedBooksState extends Equatable {
+  const FeaturedBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class FeaturedBooksInitial extends FeaturedBooksState {}
+
+final class FeaturedBooksLoading extends FeaturedBooksState {}
+
+final class FeaturedBooksSuccess extends FeaturedBooksState {
+  final List<BookModel> books;
+  const FeaturedBooksSuccess({required this.books});
+}
+
+final class FeaturedBooksfailure extends FeaturedBooksState {
+  final String error;
+
+  const FeaturedBooksfailure({required this.error});
+}
